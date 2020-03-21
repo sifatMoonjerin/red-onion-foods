701a855fc8fe51f1c08ab91b7905f552371feeeb
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-03-21 10:49:39 +0600

    routing & bootstrap initialized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
+import SignUp from './components/SignUp/SignUp';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Red Onion Foods</h1>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/signup">
+            <SignUp></SignUp>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
