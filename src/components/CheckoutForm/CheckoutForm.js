4edce5525047ebee2c44ea488bff4ef83afc6b79
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-04-05 12:32:23 +0600

    payment error/success message

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import {
   CardElement,
   useStripe,
@@ -6,6 +6,9 @@ import {
 } from '@stripe/react-stripe-js';
 
 const CheckoutForm = (props) => {
+  const [paymentError, setPaymentError] = useState(null)
+  const [paymentSuccess, setPaymentSuccess] = useState(null)
+
   const stripe = useStripe();
   const elements = useElements();
 
@@ -15,15 +18,24 @@ const CheckoutForm = (props) => {
       type: 'card',
       card: elements.getElement(CardElement),
     });
-    console.log('payment',error,paymentMethod)
+    if(error){
+      setPaymentError(error.message)
+      setPaymentSuccess(null)
+    } else{
+      setPaymentSuccess('Payment Successful')
+      setPaymentError(null)
+    }
   };
 
   return (
     <form style={{marginTop:'20px'}} onSubmit={handleSubmit}>
       <CardElement/>
+      { paymentError && <p style={{color: 'red'}}>{paymentError}</p>}
+      { paymentSuccess && <p style={{color: 'green'}}>{paymentSuccess}</p>}
       <button className="btn btn-danger address-btn" type="submit" disabled={!stripe || props.deactBtn}>
         Place Order
       </button>
+      
     </form>
   );
 };
