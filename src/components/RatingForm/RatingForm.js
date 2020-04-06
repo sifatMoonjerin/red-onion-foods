ad17df1b2b778a175ee22087cd504a5d9c82bbff
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-04-06 15:19:54 +0600

    unnecessary imports removed

--- src/components/RatingForm/RatingForm.js
+++ src/components/RatingForm/RatingForm.js
@@ -3,7 +3,6 @@ import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Modal from 'react-modal';
-import { useEffect } from 'react';
 import {useAuth} from '../useAuth/useAuth';
 
 Modal.setAppElement('#root')
